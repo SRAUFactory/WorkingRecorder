b0d6a80a2c5f28b55c76182e619a0809b7f3a776
SRAUFactory <srau.factory@gmail.com>
2020-06-22 23:04:49 +0900

    add:ソースコード追加

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func start() {
+	fmt.Println("start")
+}
+
+func stop() {
+	fmt.Println("stop")
+}
+
+func report() {
+	fmt.Println("report")
+}
