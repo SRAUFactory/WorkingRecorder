ce04d9d167b2faa19a6cb3f5ae4c498999e3ab48
SRAUFactory <srau.factory@gmail.com>
2020-06-23 23:10:00 +0900

    feat:[WIP]main関数修正＆stop関数のコマンド入力処理を追加

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	var command string
-	fmt.Scanf("%s", &command)
-	switch command {
+	flag.Parse()
+	args := flag.Args()
+	switch args[0] {
 	case "start":
 		start()
 	case "stop":
@@ -24,7 +25,22 @@ func start() {
 }
 
 func stop() {
+	var next int
 	fmt.Println("stop")
+	fmt.Println("Please select future plans!!")
+	fmt.Println("1:Suspend work")
+	fmt.Println("2:Continue to work on other tasks")
+	fmt.Println("3:Finish today's work")
+	fmt.Print("select:")
+	fmt.Scanf("%d", &next)
+	fmt.Printf("selected: %d", next)
+	fmt.Println()
+	// @ToDo 作業記録保存
+	if next == 2 {
+		start()
+	} else if next == 3 {
+		report()
+	}
 }
 
 func report() {
